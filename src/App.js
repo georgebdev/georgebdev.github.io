400daded085017b0675744ea27cf3f7a24402869
georgebdev <46300928+georgebdev@users.noreply.github.com>
2023-01-21 14:51:45 +0000

    Add chakra ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { Button, Box, Slide } from '@chakra-ui/react';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+
+  const [isOpen, setOpen] = useState(false);
+
+  function onToggle() {
+    setOpen(!isOpen)
+  }
+
   return (
-    <div className="App">
-      <p className='font-bold'>Hello World</p>
+    <div className="App mt-5">
+      <h1 className='text-6xl'>Test Website</h1>
+      <br />
+      <Button onClick={onToggle} colorScheme='teal'>Click Me</Button>
+      <Slide direction='bottom' in={isOpen} style={{ zIndex: 10 }}>
+        <Box
+          p='40px'
+          color='white'
+          mt='4'
+          bg='teal.500'
+          rounded='md'
+          shadow='md'
+        >
+          This is a test
+        </Box>
+      </Slide>
     </div>
   );
 }
